033dbac399c15167cb10a70381a944b3aad1e69d
gquiroz <gquiroz@localhost.localdomain>
2025-08-12 12:47:17 -0300

    Ejercicio 1

---
+++ TP1/ej1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Practico 1 Ejercicio 1
+echo "##### Fecha y Hora #####" >> sys_info.txt
+date >> sys_info.txt
+echo "##### ID #####" >> sys_info.txt
+id >> sys_info.txt
+echo " ##### Sistema Operativo #####" >> sys_info.txt
+uname >> sys_info.txt
+echo "##### Epacio en el disco #####" >> sys_info.txt
+df -h >> sys_info.txt
+exit
